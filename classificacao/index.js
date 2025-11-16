fcde5183a6bac6d751bc69cf5090c959198cf8a6
Taynah Saito <131884588+taynahsaito@users.noreply.github.com>
2025-11-16 09:34:28 -0300

    Improve error handling for event processing
    Handle potentially lost events from the event bus.

--- classificacao/index.js
+++ classificacao/index.js
@@ -27,11 +27,16 @@ app.post('/eventos', (req, res) => {
 
 //e colocar o servidor para executar na porta 7000
 const port = 7000
+//quando o serviço parar de ficar inoperante e voltar, é essa função que vai executar
+//async await - forma comum de tratar promises
 app.listen(port, async () => {
   console.log(`Classificação. Porta ${port}.`)
   const resp = await axios.get('http://barramento-de-eventos-service:10000/eventos')
+    //resp é um resultado da axios
+    //corpo da resposta vem associado a 'data'
+    //foreach - data é uma lista
   resp.data.forEach((valor, indice, colecao) => {
-    try{
+    try{ //nem todos os eventos que o barramento armazena são de interesse para o classificacao, estamos descartando eventos potencialmente perdidos 
       funcoes[valor.tipo](valor.dados)  
     }
     catch(err){}
